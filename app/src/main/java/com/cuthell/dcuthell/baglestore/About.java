99e153af8434a41dd4c5670dcf24819b3c013544
dcuthell <dcc1447@gmail.com>
2017-10-22 20:08:30 -0700

    update about.java

--- app/src/main/java/com/cuthell/dcuthell/baglestore/About.java
+++ app/src/main/java/com/cuthell/dcuthell/baglestore/About.java
@@ -3,16 +3,26 @@ package com.cuthell.dcuthell.baglestore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.cuthell.dcuthell.baglestore.R;
+import com.cuthell.dcuthell.baglestore.models.Review;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import butterknife.Bind;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
 public class About extends AppCompatActivity {
+
+    @Bind(R.id.locationTextView) TextView mLocationTextView;
+    @Bind(R.id.listView) ListView mListView;
+
+    public ArrayList<Review> reviews = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +44,11 @@ public class About extends AppCompatActivity {
             public void onResponse(Call call, Response response) throws IOException {
                 try{
                     String jsonData = response.body().string();
-                    Log.v("TESTING", jsonData);
+                    if(response.isSuccessful()){
+                        Log.v("TESTING", jsonData);
+                        reviews = yelpService.processResults(response);
+                    }
+
                 } catch(IOException e){
                     e.printStackTrace();
                 }
