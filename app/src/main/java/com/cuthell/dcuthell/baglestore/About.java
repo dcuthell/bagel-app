b8e09e1f49b50addfad2cd3b1a5b0d7f784450a3
dcuthell <dcc1447@gmail.com>
2017-10-22 19:05:53 -0700

    update about.java

--- app/src/main/java/com/cuthell/dcuthell/baglestore/About.java
+++ app/src/main/java/com/cuthell/dcuthell/baglestore/About.java
@@ -2,8 +2,15 @@ package com.cuthell.dcuthell.baglestore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.cuthell.dcuthell.baglestore.R;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class About extends AppCompatActivity {
 
@@ -11,5 +18,27 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        getReviews();
+    }
+
+    private void getReviews(){
+        final YelpService yelpService = new YelpService();
+        yelpService.getReviews(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try{
+                    String jsonData = response.body().string();
+                    Log.v("TESTING", jsonData);
+                } catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
