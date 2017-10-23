3fb6e5c1ec6e461f360e9d61c90e960ba3e83c7e
dcuthell <dcc1447@gmail.com>
2017-10-22 19:11:27 -0700

    add review class

---
+++ app/src/main/java/com/cuthell/dcuthell/baglestore/models/Review.java
@@ -0,0 +1,63 @@
+package com.cuthell.dcuthell.baglestore.models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by dcuthell on 10/22/2017.
+ */
+
+public class Review {
+    private String url;
+    private String text;
+    private double rating;
+    private ArrayList<String> user = new ArrayList<>();
+    private String timeCreated;
+
+    public Review(String url, String text, double rating, ArrayList<String> user, String timeCreated) {
+        this.url = url;
+        this.text = text;
+        this.rating = rating;
+        this.user = user;
+        this.timeCreated = timeCreated;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public ArrayList<String> getUser() {
+        return user;
+    }
+
+    public void setUser(ArrayList<String> user) {
+        this.user = user;
+    }
+
+    public String getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(String timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+}
