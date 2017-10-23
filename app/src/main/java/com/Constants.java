ff7d522674ec4cd818c448e4495634df419b3f08
dcuthell <dcc1447@gmail.com>
2017-10-22 18:32:27 -0700

    add constants for yelp token, update gitignore

---
+++ app/src/main/java/com/Constants.java
@@ -0,0 +1,11 @@
+package com;
+
+import com.cuthell.dcuthell.baglestore.BuildConfig;
+
+/**
+ * Created by dcuthell on 10/22/2017.
+ */
+
+public class Constants {
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+}
